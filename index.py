7107d80bdf8eba9e1c7494fc809eeb8f8ccf1748
clarapunzi <56367379+clarapunzi@users.noreply.github.com>
2019-11-13 20:45:55 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,52 @@
+import json
+import pandas as pd
+
+# Create the file 'vocabulary.json' in which each term in intro and plot is mapped to an integer (term_id) 
+
+voc = {}
+words = set() 
+for i in range(10000):
+    file = open('webpages/tsv clean/filtered_%d.tsv'%i).read().split('\n\n')[1]
+    tabs = file.split('\t')[1]+file.split('\t')[2]  # list of words in intro and plots
+    words.update(tabs.split())
+
+for term_id, term in enumerate(words):
+    voc[term]= term_id  
+    
+with open('vocabulary.json', 'w') as fp:
+    json.dump(voc, fp)
+    
+
+#  1° SEARCH ENGINE
+    
+# Create the inverted index and save it to a json file
+
+inverted_index = dict.fromkeys(range(len(voc)), [])
+for i in range(10000):
+    file = open('webpages/tsv clean/filtered_%d.tsv'%i).read()
+    lines = file.split('\n\n')[1]
+    tabs = lines.split('\t')[1]+lines.split('\t')[2]  # list of words in intro and plots
+    for word in set(tabs.split()):
+        inverted_index[voc[word]] = inverted_index[voc[word]]+[i]
+        
+# 2° SEARCH ENGINE
+
+# Create the second inverted index, that is of the type:  {term_id : [document_id, TF-IDF_{document_id, term}]] 
+
+inverted_index_freq = dict.fromkeys(range(len(voc)))
+for term_id in inverted_index_freq.keys():
+    for document_id in inverted_index[str(term_id)]:
+        if inverted_index_freq[term_id] == None:
+            inverted_index_freq[term_id] = [(document_id, tfidf(term_id, document_id))]
+        else:
+            inverted_index_freq[term_id] += [(document_id, tfidf(term_id, document_id))]
+
+# The new inverted_index_freq is as follow: {term_id : [document_id, TF-IDF_{document_id, term}]] 
+        
+        
+        
+        
+        
+        
+        
+        
---
+++ index_utils.py
@@ -0,0 +1,32 @@
+# Function that returns the data frequency (integer) of a term (string) in a specified document (i)
+
+def data_freq(term, i):
+    file = open('webpages/tsv clean/filtered_%d.tsv'%i).read().split('\n\n')[1]
+    tabs = file.split('\t')[1]+file.split('\t')[2]  # list of words in intro and plots
+    if term in tabs.split():
+        df = tabs.split().count(word)/len(tabs.split())
+    else:
+        df = 0
+    return df
+
+
+# Function that returns the inverse data frequency of a word in the vocabulary 
+
+def idf(term):
+    val = len(inverted_index[str(voc[word])])  # this is the number of documents containing the given word
+    return math.log(N/val)
+
+
+# Function that returns the term frequency - inverse document frequency 
+
+def tfidf(term_id, document_id): # (integer, integer)
+    return data_freq(get_key(term_id), document_id)*idf(get_key(term_id))
+
+
+# Function that returns the a term given its term_id
+
+def get_key(term_id): 
+    for key, value in voc.items(): 
+         if term_id == value:
+                return key   
+    return "key doesn't exist"
