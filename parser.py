8b6a9172933b05c1545a93f1c1763f0a450262fa
clarapunzi <56367379+clarapunzi@users.noreply.github.com>
2019-11-16 11:17:40 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import csv
 from bs4 import BeautifulSoup
+from parser_utils import parse_page, clean_ls
 
 # Global variables:
-
 head_wiki = ['Directed by', 'Produced by', 'Written by', 'Starring', 'Music by', 'Release date', 'Running time', 'Country', 'Language', 'Budget']
 head = ['title', 'intro', 'plot', 'film_name', 'director', 'producer', 'writer', 'starring', 'music', 'release date', 'runtime', 'country', 'language', 'budget']
 infos = ['NA']*14
@@ -27,3 +27,14 @@ for i in range(10000):
         page = open('article_%d.html' %i).read()
         soup = BeautifulSoup(page, 'html.parser')
         NOTlinks[i] = soup.title.text[:-12]
+
+        
+# The following cycle processes each tsv document and produces its 'clean' copy        
+for i in range(10000):
+    file = open('tsv/output_%d.tsv' %i, encoding = 'ISO-8859-1') 
+    lines = file.read().split('\n\n') 
+    tabs = lines[1].split('\t')   # we only process the part of the tsv files with information about the movies (not the headers)
+    with open('filtered_%d.tsv'%i,'a', encoding = 'utf-8')  as f:
+        tsv_writer = csv.writer(f, delimiter='\t')
+        tsv_writer.writerow(head) 
+        tsv_writer.writerow(clean_ls(tabs))
